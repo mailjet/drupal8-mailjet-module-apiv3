0b87aca7f20ed8c358de6c386f5cd4af06dc816e
Georgi Nikolov <gnikolov@mailjet.com>
2018-04-17 16:36:56 +0300

    Update the tracking parameter

--- src/Controller/MailjetRegisterController.php
+++ src/Controller/MailjetRegisterController.php
@@ -11,7 +11,7 @@ use Drupal\Core\Controller\ControllerBase;
 class MailjetRegisterController extends ControllerBase {
 
   public function redirect_register() {
-define("IFRAME_URL", "https://app.mailjet.com/");
-    return mailjet_go_to_external_link(IFRAME_URL. 'signup?p=drupal-3.0');
+    define("IFRAME_URL", "https://app.mailjet.com/");
+    return mailjet_go_to_external_link(IFRAME_URL . 'signup?aff=drupal-3.0');
   }
 }
